40c1c6a86d3c6ec99d6c23e16dd58f74762f3a00
Samiul Saki <samiulsaki@gmail.com>
2017-09-09 03:53:07 +0200

    Update find script @Sat  9 Sep 2017 03:53:07 CEST

--- assignment3/find.py
+++ assignment3/find.py
@@ -7,8 +7,9 @@ import re
 from collections import Counter
 
 os.system('clear')
-dir=sys.argv[2]
+
 extension=sys.argv[1]
+dir=sys.argv[2]
 #dir="/Users/samiulsaki/Documents//inf4331nsa/INF3331-aschowdh/assignment3"
 #dir1="/Users/samiulsaki/Documents/inf4331nsa/UiO-INF3331.github.io/assignments"
 
@@ -17,7 +18,7 @@ def recursive(dir):
   for root,folders,files in os.walk(dir):
     for file in files:
       if file.endswith(extension):
-        print(root,file, sep="/")
+        print(os.path.abspath(root),file,sep="/")
 
 
 recursive(dir)
