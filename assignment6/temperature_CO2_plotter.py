81cd4fad7b72fb231d51c3f1558cb99731c127a1
Samiul Saki <samiulsaki@gmail.com>
2017-11-18 05:02:05 +0100

    Some cleanup and Pydoc @ Sat Nov 18 05:02:05 CET 2017

--- assignment6/temperature_CO2_plotter.py
+++ assignment6/temperature_CO2_plotter.py
@@ -2,7 +2,9 @@
 @author: samiulsaki
 The python script with following functions:
 (1) plotify: Creates a plot with given arguments
-(2) 
+(2) plot_temperature: Temperature (globally)
+(3) plot_CO2_global: Fossil-Fuel CO2 Emissions (globally)
+(4) plot_CO2_country: Fossil-Fuel CO2 Emissions (by country)
 """
 
 import pandas as pd
@@ -34,13 +36,16 @@ globalCO2YearMin = globalCO2CSV["Year"].min()
 globalCO2YearMax = globalCO2CSV["Year"].max()
 
 def plotify(title,xLabel,yLabel,minY,maxY,minX,maxX):
+    """
+    Creates and saves the pyplot image into a BytesIO object 
+    and then returns it to thefunction
+    """
     plt.title(title)
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
     plt.legend()
     plt.xlim((minX,maxX))
     plt.ylim((minY, maxY))
-    #plt.savefig('plots/imageTemp.png')
     #plt.show()
     image = BytesIO()
     plt.savefig(image, format="png")
@@ -48,45 +53,79 @@ def plotify(title,xLabel,yLabel,minY,maxY,minX,maxX):
     return image
 
 def plot_temperature(month, start=None, end=None, minY=None, maxY=None):
+    """
+    This function plots a pyplot (scatter) of the global temperature in a given 
+    time period from given data. It returns an image/pyplot figure as a BytesIO object.
+    
+    :type month: string
+    :param month: represents the first three letters of months in calender (e.g. jan, feb, mar...)
+
+    :type start: integer
+    :param start: represents the first year in the pyplot
+
+    :type end: integer
+    :param end: represents the last year in the pyplot
+
+    :type minY: float
+    :param minY: represents the lower limit of Y axis (temperature) in the pyplot
+
+    :type maxY: float
+    :param maxY: represents the upper limit of Y axis (temperature) in the pyplot
+    """
+    # Setting the first and last year in the pyplot
     if start != None:
         start = start - tempYearMin
     if end != None:
-        end = end - tempYearMax-1
+        end = (end - tempYearMax) - 1
     month = months[month]
     tempData = globalTempCSV[start:end]
-
+    
+    # Setting the lower and upper limit of Y-axis
     if (minY != None) and (maxY != None):
         tempData = tempData[(tempData[month] >= minY) & (tempData[month] <= maxY)]
     elif minY != None:
         tempData = tempData[tempData[month] >= minY]
     elif maxY != None:
-        tempData = tempData[tempData[month] <= maxY]
+        tempData = tempData[tempData[month] <= maxY]    
     
-    
-    # Plotting the data
+    # Plotting the data with the plotify function
     plt.figure()
     plt.plot(tempData["Year"],tempData[month], "-bo", markersize=3)
     title = ("The Temperature in All of " + month)
     xLabel = "Year"
     yLabel = "Temperature ($^\circ$C)"
-    # plt.title("The Temperature in All of " + month)
-    # plt.xlabel("Year")
-    # plt.ylabel("Temperature ($^\circ$C)")
-    # plt.legend()
-    # plt.ylim((minY, maxY))
-    # #plt.savefig('plots/imageTemp.png')
-    # plt.show()
+    
+    # The plotify function is returning the image
     image = plotify(title,xLabel,yLabel,minY,maxY,minX=None,maxX=None)
     plt.close()
     return [image]
 
 def plot_CO2_global(start=None, end=None, minY=None, maxY=None):
+    """
+    This function plots a pyplot of the global CO2 emissions in a given 
+    time period from given data. It returns an image/pyplot figure as a BytesIO object.
+
+    :type start: integer
+    :param start: represents the first year in the pyplot
+
+    :type end: integer
+    :param end: represents the last year in the pyplot
+
+    :type minY: float
+    :param minY: represents the lower limit of Y axis (CO2 emission level) in the pyplot
+
+    :type maxY: float
+    :param maxY: represents the upper limit of Y axis (CO2 emission level) in the pyplot
+    """
+    
+    # Setting the first and last year in the pyplot
     if start !=None:
         start = start - globalCO2YearMin
     if end != None:
-        end = end - globalCO2YearMax-1
-
+        end = (end - globalCO2YearMax) - 1
     tempData = globalCO2CSV[start:end]
+    
+    # Setting the lower and upper limit of Y-axis
     column = "Carbon"
     if (minY != None) and (maxY != None):
         tempData = tempData[(tempData[column] >= minY) & (tempData[column] <= maxY)]
@@ -94,67 +133,74 @@ def plot_CO2_global(start=None, end=None, minY=None, maxY=None):
         tempData = tempData[tempData[column] >= minY]
     elif maxY != None:
         tempData = tempData[tempData[column] <= maxY]
-
-    # Plotting the data
+    
+    # Plotting the data with the plotify function. The plotify function 
+    # is returning the image
     plt.figure()
     plt.plot(tempData["Year"],tempData[column], "-r", markersize=3)
     title = ("The Fossil-Fuel CO2 Emissions (Globally)")
     xLabel = "Year"
-    yLabel = "Emission"
-    #plt.legend()
-    #plt.ylim((minY, maxY))
-    #plt.savefig('plots/imageCO2Global.png')
-    #plt.show()
+    yLabel = "CO2 Emission Level"
     image = plotify(title,xLabel,yLabel,minY,maxY,minX=None,maxX=None)
     plt.close()
     return [image]
 
-
 def plot_CO2_country(year, up=None, low=None):
+    """
+    This function plots a bar chart of the Fossil-Fuel CO2 emissions of individual countries at 
+    different years within the given preset threshold from given data. It returns a list of image/
+    pyplot figures as a BytesIO object.
+
+    :type year: integer
+    :param year: represents the year that is set to be plotted in the pyplot
+
+    :type up: float
+    :param up: upper boundary of the threshold (X-Axis) in the pyplot
+
+    :type low: float
+    :param low: lower boundary of the threshold (X-axis) in the pyplot
+    """
+    # Setting the year to be plotted in the pyplot
     year = str(year)
     tempData = countryCO2CSV[["Country Code", year]]
-    #print(tempData)
+    
+    # Setting the upper and lower limit of X-axis
     if (low !=None) and (up !=None):
         tempData = tempData[(tempData[year] >= low) & (tempData[year] <= up)]
     elif low !=None:
         tempData = tempData[tempData[year] >= low]
     elif up != None:
-        tempData = tempData[tempData[year] <= up]
+        tempData = tempData[tempData[year] <= up]    
+    # Setting the lower and upper limit of Y-axis
     minX = 0
     if up is None: maxX = round(1+tempData[year].max())
-    else: maxX = round(1+up)
-    
-    # Plot the data
+    else: maxX = round(1+up)    
+
+    # Setting the number of countries in each plot
     images = []
-    each_plot = 10                              # number of countries per plot
-    total_plots = (len(tempData) / each_plot)   # number of countries
-    #if total_plots == 0: total_plots = 1 #-- at least one plot must be plotted
+    each_plot = 10
+    total_plots = (len(tempData) / each_plot)
+    # Creates pyplots for given year with threshold
     for i in np.arange(0,total_plots,1):        
-        #plt.figure(figsize=(1,1))
-        #plt.subplots(figsize=(17, 10))
-        # if i == total_plots: #-- last plot
-        #     tempData[i*each_plot:].plot(x="Country Code", y=year, kind="barh")
-        # else:
+        # Set the upper and lower threshold for each individual pyplot
         start = int(i) * each_plot
         end = start + each_plot
+
+        # Plotting the data with the plotify function. The plotify function is 
+        # returning the image(s). Each image gets appended to images list
         tempData[start:end].plot(x="Country Code", y=year, kind="barh",color="black")
-        #print(tempData[start:end])
-        #plt.title ("The Fossil-Fuel CO2 Emissions (by Country)")
         title = "The Fossil-Fuel CO2 Emissions (by Country)"
-        #plt.xlabel("CO2 Emission Level (metric tons per capita)")
         xLabel = "CO2 Emission Level (metric tons per capita)"
         yLabel= "Country Code"
-        #print(start,end,min,max)
-        #plt.xlim((min,max))
         images.append(plotify(title,xLabel,yLabel,minY=None,maxY=None,minX=minX,maxX=maxX))
-        #plt.show()        
         plt.close()
     return images
 
 if __name__ == "__main__":
     """
-    The main module
+    The main call function
     """
     plot_temperature('sep')
-    plot_CO2_global()
-    plot_CO2_country(1960, up=10, low=6)
+    plot_temperature('mar', 1850, 2005, None, None)
+    plot_CO2_global(1876,1991)
+    plot_CO2_country(2000, up=10, low=6)
--- assignment6/web_visualization.py
+++ assignment6/web_visualization.py
@@ -1,25 +1,37 @@
+"""
+@author: samiulsaki
+Web Visualisation using python flask app
+"""
 import base64
 from time import time
 from flask import Flask, render_template, request
 from temperature_CO2_plotter import plot_temperature, plot_CO2_country, plot_CO2_global,\
                                     tempYearMin, tempYearMax, globalCO2YearMin, globalCO2YearMax
-
 app = Flask(__name__)
 
 def _int(input):
-	"""Converts input to 'int'. Returns None if input is an empty string."""
+	"""
+    This function returns an input in to integer value and in to None 
+    if input is an empty string.
+    """
 	if input == "":
 		return None
 	return int(input)
 
 def _float(input):
-	"""Converts input to 'float'. Returns None if input is an empty string."""
+	"""
+    This function returns an input in to float value and in to None 
+    if input is an empty string.
+    """
 	if input == "":
 		return None
 	return float(input)
 
 def _str(input):
-	"""Converts input to 'float'. Returns None if input is an empty string."""
+	"""
+    This function returns an input in to string value and in to None 
+    if input is an empty string.
+    """
 	if input == "":
 		return None
 	return str(input)
@@ -32,20 +44,12 @@ def homepage():
 def co2_global():
     if request.method == "GET":
         return render_template("co2_global_page.html", yearMin=globalCO2YearMin, yearMax=globalCO2YearMax)
-    
     start = request.form["start"]
     end = request.form["end"]
     minY = request.form["minY"]
     maxY = request.form["maxY"]
     args  = [_int(start),_int(end),_float(minY),_float(maxY)]
-    
-    #try:
     plots = plot_CO2_global(*args)
-        #print("Canari")
-    # except EmptyData_ERROR:
-    #     return render_template("co2_global_page.html", yearMin=globalCO2YearMin, yearMax=globalCO2YearMax,
-    #                             x1=start, x2=end, y1=minY, y2=maxY, 
-    #                             error="No data found! Please change the input arguments")
     plots = [base64.b64encode(p.getvalue()).decode("ascii") for p in plots]
     return render_template("co2_global_page.html", plots=plots, yearMin=globalCO2YearMin, yearMax=globalCO2YearMax, 
                             x1=start, x2=end, y1=minY, y2=maxY)
@@ -54,21 +58,13 @@ def co2_global():
 def temperature():
     if request.method == "GET":
         return render_template("temperature_page.html", yearMin=tempYearMin, yearMax=tempYearMax)
-    
     month = request.form["month"]
     start = request.form["start"]
     end = request.form["end"]
     minY = request.form["minY"]
     maxY = request.form["maxY"]
     args  = [_str(month),_int(start),_int(end),_float(minY),_float(maxY)]
-    
-    #try:
     plots = plot_temperature(*args)
-        #print("Canari")
-    # except EmptyData_ERROR:
-    #     return render_template("co2_global_page.html", yearMin=globalCO2YearMin, yearMax=globalCO2YearMax,
-    #                             x1=start, x2=end, y1=minY, y2=maxY, 
-    #                             error="No data found! Please change the input arguments")
     plots = [base64.b64encode(p.getvalue()).decode("ascii") for p in plots]
     return render_template("temperature_page.html", plots=plots, yearMin=tempYearMin, yearMax=tempYearMax, 
                             m=month,x1=start, x2=end, y1=minY, y2=maxY)
@@ -77,15 +73,11 @@ def temperature():
 def co2_country():
     if request.method =="GET":
         return render_template("co2_country_page.html", yearMin=1960, yearMax=2016)
-
     year = request.form["year"]
     up = request.form["up"]
     low = request.form["low"]
     args = [_int(year),_float(up),_float(low)]
-
-    #plots = plot_CO2_country(*args)
     plots = [base64.b64encode(p.getvalue()).decode("ascii") for p in plot_CO2_country(*args)]
-    
     return render_template("co2_country_page.html", plots=plots, yearMin=1960, yearMax=2016, 
                             y = year, b1=up, b2=low)
 
